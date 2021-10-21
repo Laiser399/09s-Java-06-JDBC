f001a45a2fffa0f87dc17a94224cc205bcc41741
Laiser399 <Laiser399@mail.ru>
2021-10-21 19:33:58 +0300

    entities

---
+++ src/main/java/org/mai/library/entities/Book.java
@@ -0,0 +1,30 @@
+package org.mai.library.entities;
+
+/**
+ * Created by Asus on 10/21/2018.
+ */
+public class Book {
+    private final int id;
+    private final String title;
+
+    public Book(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
