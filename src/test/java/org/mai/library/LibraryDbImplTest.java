39166b8f6279a4eabc6f15706c2f4f601aa34d9e
Laiser399 <Laiser399@mail.ru>
2021-10-23 16:25:59 +0300

    library tests

---
+++ src/test/java/org/mai/library/LibraryDbImplTest.java
@@ -0,0 +1,143 @@
+package org.mai.library;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mai.library.entities.Book;
+import org.mai.library.entities.Student;
+
+import java.sql.DriverManager;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class LibraryDbImplTest {
+    private static final String connectionString = "jdbc:h2:mem:library";
+
+    private LibraryDbImpl library;
+
+    private final Book book1 = new Book(1, "bored book");
+    private final Book book2 = new Book(2, "useful book");
+    private final Book book3 = new Book(3, "Теория всего");
+    private final Book duplicatedBook1Id = new Book(1, "This is wrong book");
+    private final Book book1SameName = new Book(4, "bored book");
+    private final Book nonExistingBook = new Book(399, "aga");
+
+    private final Student student1 = new Student(1, "Mike");
+    private final Student student2 = new Student(2, "Jordan");
+    private final Student student3 = new Student(3, "Emily");
+    private final Student duplicatedStudent1Id = new Student(1, "noname");
+    private final Student student1SameName = new Student(4, "Mike");
+    private final Student nonExistingStudent = new Student(399, "who?");
+
+    @Before
+    public void setUp() throws Exception {
+        var connection = DriverManager.getConnection(connectionString);
+        App.createTables(connection);
+        library = new LibraryDbImpl(connection);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        library.close();
+        library = null;
+    }
+
+    @Test
+    public void addNewBook() {
+        assertThat(library.addNewBook(book1), equalTo(true));
+        assertThat(library.addNewBook(duplicatedBook1Id), equalTo(false));
+        assertThat(library.addNewBook(book1SameName), equalTo(true));
+        assertThat(library.addNewBook(book2), equalTo(true));
+        assertThat(library.addNewBook(book3), equalTo(true));
+    }
+
+    @Test
+    public void addStudent() {
+        assertThat(library.addStudent(student1), equalTo(true));
+        assertThat(library.addStudent(duplicatedStudent1Id), equalTo(false));
+        assertThat(library.addStudent(student1SameName), equalTo(true));
+        assertThat(library.addStudent(student2), equalTo(true));
+        assertThat(library.addStudent(student3), equalTo(true));
+    }
+
+    @Test
+    public void borrowBook() {
+        addBooks();
+        addStudents();
+
+        assertThat(library.borrowBook(nonExistingBook, student1), equalTo(false));
+        assertThat(library.borrowBook(book1, nonExistingStudent), equalTo(false));
+
+        assertThat(library.borrowBook(book1, student1), equalTo(true));
+        assertThat(library.borrowBook(book1, student1), equalTo(false));
+        assertThat(library.borrowBook(book1, student2), equalTo(false));
+
+        assertThat(library.borrowBook(book2, student1), equalTo(true));
+        assertThat(library.borrowBook(book3, student2), equalTo(true));
+    }
+
+    @Test
+    public void returnBook() {
+        addBooks();
+        addStudents();
+
+        assertThat(library.returnBook(book1, student1), equalTo(false));
+
+        library.borrowBook(book1, student1);
+
+        assertThat(library.returnBook(book1, student2), equalTo(false));
+        assertThat(library.returnBook(book1, nonExistingStudent), equalTo(false));
+        assertThat(library.returnBook(book1, student1), equalTo(true));
+        assertThat(library.returnBook(book1, student1), equalTo(false));
+    }
+
+    @Test
+    public void findAvailableBooks() {
+        addBooks();
+        addStudents();
+
+        var availableBooks = library.findAvailableBooks();
+        assertThat(availableBooks, allOf(
+                hasItems(book1, book2, book3),
+                not(hasItems(book1SameName, duplicatedBook1Id))));
+
+        library.borrowBook(book1, student1);
+        library.borrowBook(book2, nonExistingStudent);
+        library.returnBook(book1, student2);
+        library.returnBook(book2, student2);
+
+        availableBooks = library.findAvailableBooks();
+        assertThat(availableBooks, allOf(
+                hasItems(book2, book3),
+                not(hasItem(book1))));
+
+        library.returnBook(book1, student1);
+
+        availableBooks = library.findAvailableBooks();
+        assertThat(availableBooks, hasItems(book1, book2, book3));
+    }
+
+    @Test
+    public void getAllStudents() {
+        var students = library.getAllStudents();
+        assertThat(students.size(), equalTo(0));
+
+        addStudents();
+
+        students = library.getAllStudents();
+        assertThat(students, hasItems(student1, student2, student3));
+    }
+
+    private void addBooks() {
+        library.addNewBook(book1);
+        library.addNewBook(book2);
+        library.addNewBook(book3);
+    }
+
+    private void addStudents() {
+        library.addStudent(student1);
+        library.addStudent(student2);
+        library.addStudent(student3);
+    }
+}
